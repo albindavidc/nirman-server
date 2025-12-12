c629567513f42ecf0038acebbe64710793a97104
Albin David C <albindavidc007@gmail.com>
2025-12-12 15:01:11 +0530

    created prisma and auth models

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,37 @@
+import { PrismaClient } from '../generated/client/client';
+import {
+  Injectable,
+  OnModuleInit,
+  OnModuleDestroy,
+  Logger,
+} from '@nestjs/common';
+
+import 'dotenv/config'; // Ensure env vars are loaded
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  private readonly logger = new Logger(PrismaService.name);
+
+  constructor() {
+    super({
+      log: [],
+    });
+  }
+
+  async onModuleInit() {
+    try {
+      await this.$connect();
+      this.logger.log('🚀 Prisma connected to MongoDB');
+    } catch (error) {
+      this.logger.error('❌ Prisma connection failed:', error);
+    }
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+    this.logger.log('🛑 Prisma disconnected');
+  }
+}
