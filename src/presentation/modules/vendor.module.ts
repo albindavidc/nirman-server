ee2a9def8566168ff158a923d1d04b6aac88679a
Albin David C <albindavidc007@gmail.com>
2026-02-19 03:04:19 +0530

    fix: update the add vendor logic

--- src/presentation/modules/vendor.module.ts
+++ src/presentation/modules/vendor.module.ts
@@ -4,9 +4,11 @@ import { CqrsModule } from '@nestjs/cqrs';
 // Infrastructure
 import { PrismaModule } from '../../infrastructure/prisma/prisma.module';
 import { VendorRepository } from '../../infrastructure/repositories/vendor.repository';
+import { UserRepository } from '../../infrastructure/repositories/user.repository';
 
 // Domain interfaces
 import { VENDOR_REPOSITORY } from '../../domain/repositories/vendor-repository.interface';
+import { USER_REPOSITORY } from '../../domain/repositories/user-repository.interface';
 
 // Controllers
 import { VendorManagementController } from '../controllers/vendor-management.controller';
@@ -16,6 +18,7 @@ import { GetVendorsHandler } from '../../application/handlers/queries/vendor/get
 import { GetVendorByIdHandler } from '../../application/handlers/queries/vendor/get-vendor-by-id.handler';
 
 // Command Handlers
+import { CreateVendorByAdminHandler } from '../../application/handlers/commands/vendor/create-vendor-by-admin.handler';
 import { UpdateVendorHandler } from '../../application/handlers/commands/vendor/update-vendor.handler';
 import { UnblacklistVendorHandler } from '../../application/handlers/commands/vendor/unblacklist-vendor.handler';
 import { RejectVendorHandler } from '../../application/handlers/commands/vendor/reject-vendor.handler';
@@ -23,6 +26,7 @@ import { RequestRecheckHandler } from '../../application/handlers/commands/vendo
 
 const QueryHandlers = [GetVendorsHandler, GetVendorByIdHandler];
 const CommandHandlers = [
+  CreateVendorByAdminHandler,
   UpdateVendorHandler,
   UnblacklistVendorHandler,
   RejectVendorHandler,
@@ -34,6 +38,7 @@ const CommandHandlers = [
   controllers: [VendorManagementController],
   providers: [
     { provide: VENDOR_REPOSITORY, useClass: VendorRepository },
+    { provide: USER_REPOSITORY, useClass: UserRepository },
     ...QueryHandlers,
     ...CommandHandlers,
   ],
